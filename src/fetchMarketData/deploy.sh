366b8269f1f17a75a19c7f1a275c712b10dffc64
majurageerthan <majuran.thasan@gmail.com>
2023-02-22 11:42:07 +0530

    Automatic dev and prod deployment config

--- src/fetchMarketData/deploy.sh
+++ src/fetchMarketData/deploy.sh
@@ -1,7 +1,9 @@
 export AWS_ACCESS_KEY_ID=AKIASJAOCGSKSZDLRSPC
 export AWS_SECRET_ACCESS_KEY=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
 # AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are now available for serverless to use
+cp .env.prod .env
 rm -rf .serverless/
+sleep 2
 serverless deploy --stage production
 # serverless remove --stage production
 
--- src/fetchMarketData/runLocally.sh
+++ src/fetchMarketData/runLocally.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 # git config user.name
 # git config user.email
-
+cp .env.local .env
+sleep 2
 serverless invoke local --function fetchStockDataAndUpdate
 
 
